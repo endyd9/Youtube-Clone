d55b73753488aa2af4da999090eabdb99e6fbae7
endyd9 <endyd9@gmail.com>
2023-04-24 00:33:25 +0900

    7일차2

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -102,7 +102,6 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    console.log(userData);
     const emailData = await (
       await fetch(`${apiUrl}/user/emails`, {
         headers: {
@@ -110,13 +109,31 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    const email = emailData.find(
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!email) {
+    if (!emailObj) {
       return res.redirect("/login");
     }
-    return;
+    console.log(emailObj.email);
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      return res.redirect("/");
+    } else {
+      const user = await User.create({
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect("/");
+    }
   } else {
     return res.redirect("/login");
   }
@@ -124,5 +141,8 @@ export const finishGithubLogin = async (req, res) => {
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
-export const logout = (req, res) => res.send("Log Out");
+export const logout = (req, res) => {
+  req.session.loggedIn = false;
+  return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
