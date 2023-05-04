d5dfe1df8dd6a457a468a5726d653cf635756890
endyd9 <endyd9@gmail.com>
2023-05-05 00:16:35 +0900

    11일차

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,3 @@
 import "../scss/styles.scss";
 
-console.log("Hello!!");
+console.log("main");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -18,7 +18,7 @@ videoRouter
   .get(getEdit)
   .post(postEdit);
 videoRouter
-  .route("/:id([0-9a-f]{24})/")
+  .route("/:id([0-9a-f]{24})/delete")
   .all(protectorMiddleware)
   .get(deleteVideo);
 videoRouter
